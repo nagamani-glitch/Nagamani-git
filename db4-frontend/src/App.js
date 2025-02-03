2be1cc0ff530f403e0e34f29a95202725f0e585d
sangeeta <sanjuother107@gmail.com>
2025-02-03 15:02:46 +0530

    SAngeeta

--- db4-frontend/src/App.js
+++ db4-frontend/src/App.js
@@ -8,24 +8,7 @@ import MainScreen from './screens/mainScreen/MainScreen';
 // import AuthScreen from './screens/authScreens/AuthScreen';
 import LoginPage from './screens/authScreens/loginScreen/LoginPage';
 import RegisterPage from './screens/authScreens/registerScreen/RegisterPage';
-import MyWorkspace from './screens/myWorkspace/MyWorkspace';
-import Dashboards from './screens/dashboards/Dashboards';
-import Directories from './screens/directories/Directories';
 import HomeScreen from './screens/HomeScreen';
-// import MyTeam from './screens/myTeam/MyTeam';
-import Favourites from './screens/favourites/Favourites';
-import TeamPerformance from './screens/teamPerformance/TeamPerformance';
-import Talent from './screens/talent/Talent';
-import TimeOff from './screens/timeOff/TimeOff';
-import SpendManagement from './screens/spendManagement/SpendManagement';
-import Benefits from './screens/benefits/Benefits';
-import Pay from './screens/pay/Pay';
-import BigData from './screens/bigData/BigData';
-import Expenses from './screens/expenses/Expenses';
-import Birthdays from './screens/birthdays/Birthdays';
-import Anniversaries from './screens/anniversaries/Anniversaries';
-import EmployeesScreen from './screens/EmployeesScreen';
-import RegisterScreen from './screens/RegisterScreen';
 import Dashboard from './screens/templates/Dashboard';
 import PrivateRoute from './screens/PrivateRoute';
 import { Container } from 'react-bootstrap';
@@ -50,31 +33,12 @@ function App() {
 
             <Route path="/forgot-password" element={<ForgotPassword />} />
             <Route path='/reset-password/:token' element={<ResetPassword />} />
-
-
             <Route path='/register' element={<RegisterPage />} />
             <Route path='/verifyOtp' element={<VerifyOtpPage />} />
             <Route element={<PrivateRoute />}>
               <Route path='/home' element={<HomeScreen />} />
-              <Route path='/My Team' element={<EmployeesScreen />} />
-              <Route path='/My Workspace' element={<MyWorkspace />} />
               <Route path='/Dashboards' element={<Dashboard />} />
-              <Route path='/Directory' element={<Directories />} />
-              <Route path='/Favorites' element={<Favourites />} />
-              <Route path='/Team Performance' element={<TeamPerformance />} />
-              <Route path='/Talent' element={<Talent />} />
-              <Route path='/Time Off' element={<TimeOff />} />
-              <Route path='/Spend Management' element={<SpendManagement />} />
-              <Route path='/Benefits' element={<Benefits />} />
-              <Route path='/Pay' element={<Pay />} />
-              <Route path='/Big Data Analytics' element={<BigData />} />
-              <Route path='/Expenses' element={<Expenses />} />
-              <Route path='/Birthdays' element={<Birthdays />} />
-              <Route path='/Anniversaries' element={<Anniversaries />} />
-              <Route path='/Personal Information' element={<RegisterScreen />} />
-              <Route path="/faq/:categoryId" element={<FaqPage />} />
             </Route>
-            <Route path='/hr' element={<Dashboards />} />
           </Routes>
         </Container>
         </SidebarProvider>
