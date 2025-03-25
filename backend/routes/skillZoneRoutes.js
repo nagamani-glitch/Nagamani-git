a43417ac6443044f65d8516debedc0b3577eed16
Subikshan0612 <subishan1234@gmail.com>
2025-03-25 12:58:22 +0530

    Subikshan

--- backend/routes/skillZoneRoutes.js
+++ backend/routes/skillZoneRoutes.js
@@ -1,24 +1,3 @@
-// import express from 'express'
-// const router = express.Router();
-// import {
-//   getSkillZones,
-//   createSkillZone,
-//   updateSkillZoneCandidate,
-//   deleteSkillZoneCandidate,
-//   deleteSkillZone // include deleteSkillZone
-// } from '../controllers/skillZoneController.js';
-
-// // Routes
-// router.get('/', getSkillZones); // Fetch all skill zones
-// router.post('/', createSkillZone); // Create new skill zone
-// router.put('/:skillZoneId/candidates/:candidateId', updateSkillZoneCandidate); // Edit candidate in a skill zone
-// router.delete('/:skillZoneId/candidates/:candidateId', deleteSkillZoneCandidate); // Delete candidate from a skill zone
-
-// // New route to delete an entire skill zone
-// router.delete('/:skillZoneId', deleteSkillZone); // Delete skill zone by ID
-
-// export default router
-
 import express from 'express';
 import {
   getAllSkills,
