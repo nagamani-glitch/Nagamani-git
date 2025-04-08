d8355296df75305c67505969fb59042598e3d9d7
Subikshan0612 <subishan1234@gmail.com>
2025-04-08 12:08:02 +0530

    Recruitment Candidate Updated

--- backend/models/ApplicantProfile.js
+++ backend/models/ApplicantProfile.js
@@ -1,26 +1,3 @@
-// // models/ApplicantProfile.js
-// import mongoose from 'mongoose';
-
-// const applicantProfileSchema = new mongoose.Schema({
-//   name: String,
-//   email: String,
-//   position: String,
-//   status: {
-//     type: String,
-//     enum: ['Hired', 'Not-Hired'],
-//     default: 'Not-Hired',
-//   },
-//   color: {
-//     type: String,
-//     default: '#ff9800',
-//   },
-// });
-
-// const ApplicantProfile = mongoose.model('ApplicantProfile', applicantProfileSchema);
-
-// export default ApplicantProfile
-
-// models/ApplicantProfile.js
 import mongoose from 'mongoose';
 
 const applicantProfileSchema = new mongoose.Schema({
